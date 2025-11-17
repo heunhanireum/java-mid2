b8c3f71506172d756904b6327cb8351841490a61
minji <heunhanireum@gmail.com>
2025-11-17 12:56:27 +0900

    251117

---
+++ src/generic/ex5/EraserBox.java
@@ -0,0 +1,13 @@
+package generic.ex5;
+
+public class EraserBox<T> {
+
+    public boolean instanceCheck(Object param) {
+        //return param instanceof T; //컴파일 오류 -> T가 런타임에서 Object로 바뀌어버림 -> 타입 매개변수의 instanceof를 허용하지 X
+        return false
+    }
+
+    public void create() {
+        //return new T(); //컴파일 오류 -> T가 런타임에서 Object로 바뀌어버림 -> 타입 매개변수의 new를 허용하지 X
+    }
+}
